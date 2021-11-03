f07acf9d830b0048f45d06a3332c879df9176cb8
Akash Anand <akash10.anand@gmail.com>
2021-11-03 13:49:05 +0530

    Made Header common

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 import { Switch, Route } from "react-router-dom";
 import KBoard from "./pages/KBoard";
 import TaskScreen from "./pages/TaskScreen";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route path="/" exact={true}>
           <KBoard />
--- src/pages/KBoard.tsx
+++ src/pages/KBoard.tsx
@@ -1,11 +1,7 @@
 import Board from '../components/Board';
-import Header from '../components/Header';
 
 export default function kanbanBoard() {
   return (
-    <>
-      <Header />
       <Board />
-    </>
   )
 }
